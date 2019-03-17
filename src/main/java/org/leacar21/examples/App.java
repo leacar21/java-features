7658bd0d54e9f090d297d8836b684f5d1bbe4eb3
Leandro Carrasco <lcarrasco@despegar.com>
2019-03-16 21:45:03 -0300

    Commit inicial

---
+++ src/main/java/org/leacar21/examples/App.java
@@ -0,0 +1,13 @@
+package org.leacar21.examples;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
